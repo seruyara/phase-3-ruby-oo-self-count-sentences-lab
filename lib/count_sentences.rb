0f49175101f0f1ded89e026e38fee093906b3e21
Sheila Seruya <Sheilaseruya@gmail.com>
2023-05-21 17:31:30 +0300

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
+    
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]/)
+    sentences.reject!(&:empty?)
+    sentences.length
   end
 end
